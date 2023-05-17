2db139620136d39e92bbda124c94fd5b8fc761a2
jfmarkan <jfmarkan@gmail.com>
2023-05-17 15:18:42 +0200

    work done!

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let player1 = Math.floor(Math.random() *6) + 1;
+let player2 = Math.floor(Math.random() *6) + 1;
+
+if (player1 > player2){
+    console.log(player1);
+    console.log(player2);
+    console.log('player one wins');
+} else if (player1 < player2){
+    console.log(player1);
+    console.log(player2);
+    console.log('player two wins');
+} else {
+    console.log(player1);
+    console.log(player2);
+    console.log("It's a tie");
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+
+let userMail;
+let mailList = ["jfmarkan@gmail.com", 'panchitomarkan@gmail.com', 'boolean@gmail.com', 'riccardo@gmail.com'];
+let result = false;
+
+userMail = prompt('Insert your e-mail');
+console.log(userMail);
+
+for ( i = 0 ; i < mailList.length ; i++){
+    if (mailList[i] === userMail){
+        result = true;
+        console.log(result);
+    } 
+}
+
